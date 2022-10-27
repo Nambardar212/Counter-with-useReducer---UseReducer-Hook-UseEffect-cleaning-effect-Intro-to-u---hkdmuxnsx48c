4b57b87e299387ad9cd893434d7ae5803de056aa
Nambardar212 <nikhilsaini0210@gmail.com>
2022-10-27 15:25:42 +0530

    1st

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 import React, { useReducer } from 'react'
 import { counterReducer } from '../reducers/counterReducer';
 import '../styles/App.css';
+const initalState = {count: 0}
+
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+const [state,dispatch] = useReducer(counterReducer, initalState)
+
+
+
+const handleIncrease = (e) => {
+    dispatch({
+      type : e.target.innerText
+    })
+}
+
+const handleDecrease = (e) => {
+  dispatch({
+    type : e.target.innerText
+  })
+}
+
   return (
     <div id="main">
-
+          <span id='counter'>{state.count}</span><br/>
+          <button id='increment-btn' onClick={(e) => handleIncrease(e)}>increase</button>
+          <button id='decrement-btn' onClick={(e) => handleDecrease(e)}>decrease</button>
     </div>
   )
 }
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,13 @@
 const counterReducer = (state,action) =>{
 
+        if(action.type === 'increase') {
+            return {count: state.count + 1}
+        }
+        if(action.type === 'decrease') {
+          return {count : state.count - 1}
+        }
+      
+
 }
 
 export {counterReducer}
